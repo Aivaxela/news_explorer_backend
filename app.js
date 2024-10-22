92e55ad4ecce44d531e5476df9134df1be39a341
Riley Marcum <rmarcum13@gmail.com>
2024-10-21 20:58:02 -0400

    add router infastructure

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+mongoose.set("strictQuery", true);
 mongoose.connect("mongodb://127.0.0.1:27017/newsexplorer_db");
 
 const { PORT = 3002 } = process.env;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.listen(PORT);
+app.listen(PORT, () =>
+  console.log(`Server is running at http://localhost:${PORT}/`)
+);
 
 app.use("/users", require("./routes/users"));
-
+app.use("/", require("./routes/index"));
--- models/user.js
+++ models/user.js
@@ -3,26 +3,28 @@ const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        validate: {
-            validator(value) {
-                return validator.isEmail(value);
-            },
-            message: "You must enter a valid email address"
-        }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: "You must enter a valid email address",
     },
-    password: {
-        type: String,
-        required: true,
-        select: false,
-    },
-    name: {
-        type: String,
-        minlength: 2,
-        maxlength: 30,
-        required: true
-    }
-})
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
