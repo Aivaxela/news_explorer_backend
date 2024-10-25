6c56818f26837acfdf52619937a82838853bbfb3
Aivaxela <rmarcum13@gmail.com>
2024-10-25 12:39:00 -0400

    add loggers and rate limiter

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const helmet = require("helmet");
+const { limiter } = require("./middleware/limiter");
+const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middleware/logger");
 
 mongoose.set("strictQuery", true);
 mongoose.connect("mongodb://127.0.0.1:27017/newsexplorer_db");
@@ -14,6 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
+app.use(limiter);
+app.use(requestLogger);
 
 app.listen(PORT, () =>
   console.log(`Server is running at http://localhost:${PORT}/`)
@@ -22,3 +27,6 @@ app.listen(PORT, () =>
 app.use("/users", require("./routes/users"));
 app.use("/articles", require("./routes/articles"));
 app.use("/", require("./routes/index"));
+
+app.use(errorLogger);
+app.use(errors());
---
+++ middleware/limiter.js
@@ -0,0 +1,13 @@
+const { rateLimit } = require("express-rate-limit");
+
+function handleLimitReached() {
+  return Promise.reject(new Error("too many requests"));
+}
+
+module.exports.limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, //1 minutes
+  limit: 10,
+  standardHeaders: "draft-7",
+  legacyHeaders: false,
+  handler: handleLimitReached,
+});
