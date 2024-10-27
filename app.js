9e6483502f5a6e88667ccc9bdef5099f746ae248
Riley Marcum <rmarcum13@gmail.com>
2024-10-27 15:19:25 -0400

    add db address and port to config

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const { limiter } = require("./middleware/limiter");
 const { errors } = require("celebrate");
 const { requestLogger, errorLogger } = require("./middleware/logger");
 const { errorHandler, errorSender } = require("./middleware/error-handler");
+const { DB_ADDRESS, DB_PORT } = require("./utils/config");
 
 mongoose.set("strictQuery", true);
-mongoose.connect("mongodb://127.0.0.1:27017/newsexplorer_db");
+mongoose.connect(DB_ADDRESS);
 
-const { PORT = 3002 } = process.env;
 const app = express();
 
 app.use(express.json());
@@ -29,4 +29,4 @@ app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler, errorSender);
 
-app.listen(PORT);
+app.listen(DB_PORT);
